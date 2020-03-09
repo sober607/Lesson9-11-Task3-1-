04b0ba86372c12dd32a3fcb6a949bc14df5a62d3
sober607 <55458570+sober607@users.noreply.github.com>
2020-03-09 15:14:18 +0200

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    public class MyClass
+    {
+        public string name;
+        public string surname;
+        public MyClass (string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+}
---
+++ Lesson9(11)Task3(1)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyNamespace;
+
+namespace Lesson9_11_Task3_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass newVariable = new MyClass("Viktor", "Popov");
+            Console.ReadLine();
+        }
+    }
+}
